928d6976de86f42713e34bd16b74942a3cba9314
MicoNoNico <56491842+MicoNoNico@users.noreply.github.com>
2023-02-28 10:49:33 +0100

    map modes and tween animations

--- Assets/Scripts/User Interface/UiNavigation.cs
+++ Assets/Scripts/User Interface/UiNavigation.cs
@@ -7,12 +7,17 @@ using UnityEngine.UI;
 
 public class UiNavigation : MonoBehaviour
 {
-    [Header("Buttons")]
+    [Header("Tab Buttons")]
     public GameObject eventsBtn;
     public GameObject settingsBtn;
     public GameObject activesBtn;
     public GameObject propsBtn;
     public GameObject eventPrefab;
+    
+    [Header("Map Mode Buttons")]
+    public GameObject exploreViewBtn;
+    public GameObject editViewBtn;
+    public GameObject mapViewBtn;
 
     [Header("Panels")] 
     public GameObject eventsPnl;
@@ -25,23 +30,23 @@ public class UiNavigation : MonoBehaviour
     {
         SetOpacity(settingsBtn, 0.3f, 0.2f);
         SetOpacity(eventsBtn, 1f, 0.2f);
-        
-        eventsPnl.SetActive(true);
-        settingsPnl.SetActive(false);
+
+        MoveObj(settingsPnl, new Vector2(460 , 0), 0.3f);
+        MoveObj(eventsPnl, new Vector2(0 , 0), 0.3f);
     }
     
     public void SwitchToSettings()
     {
-        SetOpacity(eventsBtn, 0.3f, 0.2f);
+        SetOpacity(eventsBtn, 0.8f, 0.2f);
         SetOpacity(settingsBtn, 1f, 0.2f);
-        
-        eventsPnl.SetActive(false);
-        settingsPnl.SetActive(true);
+
+        MoveObj(settingsPnl, new Vector2(0 , 0), 0.3f);
+        MoveObj(eventsPnl, new Vector2(-460 , 0), 0.3f);
     }
     
     public void SwitchToActives()
     {
-        SetOpacity(propsBtn, 0.3f, 0.2f);
+        SetOpacity(propsBtn, 0.8f, 0.2f);
         SetOpacity(activesBtn, 1f, 0.2f);
         
         activesPnl.SetActive(true);
@@ -50,16 +55,43 @@ public class UiNavigation : MonoBehaviour
     
     public void SwitchToProps()
     {
-        SetOpacity(activesBtn, 0.3f, 0.2f);
+        SetOpacity(activesBtn, 0.8f, 0.2f);
         SetOpacity(propsBtn, 1f, 0.2f);
         
         activesPnl.SetActive(false);
         propsPnl.SetActive(true);
     }
+    
+    //Map Modes
+    public void SwitchToExploreMode()
+    {
+        SetOpacity(exploreViewBtn, 1f, 0.2f);
+        SetOpacity(editViewBtn, 0.7f, 0.2f);
+        SetOpacity(mapViewBtn, 0.7f, 0.2f);
+    }
+    
+    public void SwitchToEditMode()
+    {
+        SetOpacity(exploreViewBtn, 0.7f, 0.2f);
+        SetOpacity(editViewBtn, 1f, 0.2f);
+        SetOpacity(mapViewBtn, 0.7f, 0.2f);
+    }
+    
+    public void SwitchToMapMode()
+    {
+        SetOpacity(exploreViewBtn, 0.7f, 0.2f);
+        SetOpacity(editViewBtn, 0.7f, 0.2f);
+        SetOpacity(mapViewBtn, 1f, 0.2f);
+    }
 
     private void SetOpacity(GameObject element, float opacity, float time)
     {
         element.GetComponent<Image>().DOFade(opacity, time);
+    }
+
+    private void MoveObj(GameObject element, Vector2 destination ,float time)
+    {
+        element.GetComponent<RectTransform>().DOAnchorPos(destination, time);
     }
     
     //Instantiate event button prefab
