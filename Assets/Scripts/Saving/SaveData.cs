b9a76498de70f6edd0958c2680df68b46c1239f2
Fadi Nahhas <fadi.nahhas1@gmail.com>
2023-03-21 13:41:31 +0100

    Fixed menu scaling

---
+++ Assets/Scripts/Saving/SaveData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Saving
+{
+    public class SaveData
+    {
+        
+    }
+
+    public class DecorativeObjectData
+    {
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public string MeshName;
+    }
+}
