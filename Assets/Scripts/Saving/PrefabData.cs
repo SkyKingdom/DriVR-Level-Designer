871d83f9fd3696043fe2944c79133daac8a6b26c
Fadi Nahhas <fadi.nahhas1@gmail.com>
2023-03-21 14:27:48 +0100

    Prefabs Data Object

---
+++ Assets/Scripts/Saving/PrefabData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Saving
+{
+    [CreateAssetMenu(fileName = "PrefabData", menuName = "Saving/PrefabData", order = 0)]
+    public class PrefabData : ScriptableObject
+    {
+        [SerializeField] private PrefabDataDictionary prefabsDictionary;
+        
+        public IDictionary<int, GameObject> PrefabsDictionary
+        {
+            get { return prefabsDictionary; }
+            set { prefabsDictionary.CopyFrom(value); }
+        }
+    }
+}
