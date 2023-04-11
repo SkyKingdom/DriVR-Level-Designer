4b12a5dedc25f02031bf93fd88a75e03968dd95d
MicoNoNico <56491842+MicoNoNico@users.noreply.github.com>
2023-04-11 15:00:39 +0200

    Tutorial v1

--- Assets/Scripts/User Interface/MenuNav.cs
+++ Assets/Scripts/User Interface/MenuNav.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
+using DG.Tweening;
 
 public class MenuNav : MonoBehaviour
 {
     public Animator hammer;
+
+    [Header("Tutorial")] 
+    public TextMeshProUGUI tutorialText;
+    public List<GameObject> tutorialMasks;
+    public GameObject tutorialPanel;
+
+    private int _tutorialCounter = 0;
 
     private bool _animRunning = false;
 
@@ -42,4 +51,63 @@ public class MenuNav : MonoBehaviour
 
         _animRunning = false;
     }
+    
+    //tutorial
+
+    public void GuideButtonPress()
+    {
+        tutorialPanel.SetActive(true);
+        
+        tutorialPanel.transform.DOScale(new Vector3(1.05f, 1.05f, 1.05f), 0.2f)
+            .OnComplete(() => tutorialPanel.transform.DOScale(new Vector3(1f, 1f, 1f), 0.2f));
+    }
+
+    public void CloseButtonPress()
+    {
+        tutorialPanel.SetActive(false);
+        tutorialPanel.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+    }
+    
+    public void NextButtonPress()
+    {
+        if (_tutorialCounter > tutorialMasks.Count-1)
+        {
+            _tutorialCounter = 0;
+        }
+        
+        foreach (var mask in tutorialMasks)
+        {
+            mask.SetActive(false);
+        }
+        
+        tutorialMasks[_tutorialCounter].SetActive(true);
+        SetTutorialText(_tutorialCounter);
+        _tutorialCounter++;
+    }
+
+    private void SetTutorialText(int counter)
+    {
+        switch (counter)
+        {
+            case 0:
+                tutorialText.SetText("toggle the map to accurately adjust object positions");
+                break;
+            case 1:
+                tutorialText.SetText("switch between map modes");
+                break;
+            case 2:
+                tutorialText.SetText("switch between various object types to place in your level");
+                break;
+            case 3:
+                tutorialText.SetText("adjust object settings based on the type of object selected");
+                break;
+            case 4:
+                tutorialText.SetText("make sure to save the changes to your object before saving the level");
+                break;
+            case 5:
+                tutorialText.SetText("once your level is done, you may choose to save it to your computer or publish it");
+                break;
+        }
+    }
+    
 }
