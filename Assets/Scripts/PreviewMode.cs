2a34940c9d9778d011a4a805668dfdb494870f3e
MicoNoNico <56491842+MicoNoNico@users.noreply.github.com>
2023-05-23 16:53:30 +0200

    preview mode

--- Assets/Scripts/PreviewMode.cs
+++ Assets/Scripts/PreviewMode.cs
@@ -2,25 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PreviewMode : MonoBehaviour
 {
-    private bool _isPreviewMode = false;
+    private bool IsPreviewMode => LevelGeneratorManager.Instance.Mode == Mode.FirstPerson;
+    [SerializeField] private InputActionReference escapeAction;
 
-    private void Update()
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            _isPreviewMode = !_isPreviewMode;
-            if (_isPreviewMode)
-            {
-                LevelGeneratorManager.Instance.SetMode(3);
-            }
-            else
-            {
-                LevelGeneratorManager.Instance.SetMode(1);
-            }
-        }
+        escapeAction.action.Enable();
+        escapeAction.action.performed += OnEscapePressed;
+    }
+
+    private void OnDisable()
+    {
+        escapeAction.action.Disable();
+        escapeAction.action.performed -= OnEscapePressed;
+    }
+
+    private void OnEscapePressed(InputAction.CallbackContext obj)
+    {
+        if (!IsPreviewMode)
+            return;
+        TogglePreview();
+    }
+
+
+    public void TogglePreview()
+    {
+        LevelGeneratorManager.Instance.SetMode(IsPreviewMode ? 1 : 3);
     }
 
 }
